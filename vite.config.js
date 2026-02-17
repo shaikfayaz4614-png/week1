6c2b9edf874c077a48fb1c287a7cc56854c61c7f
shaikfayaz4614-png <shaikfayaz4614@gmail.com>
2026-02-17 10:46:52 +0530

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: 'week1', //YourRepoName
+  base: '/week1/', //YourRepoName
 })
